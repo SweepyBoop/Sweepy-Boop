c92a8e4e6ce013e5ab58eb9a2f88f210e08bafa1
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2022-06-28 18:16:09 +0900

    Update UtilsWA.lua

--- UtilsWA.lua
+++ UtilsWA.lua
@@ -130,7 +130,7 @@ end
 -- Check whether spell is enabled for UNIT_SPELLCAST_ events
 local function unitSpellEnabled(spell, unitId)
     -- Check if opponent is arena
-    if ( spell.trackType == NS.TRACK_UNIT ) and ( not NS.isUnitArena(unitId) ) then return end
+    if ( spell.trackType == TRACK_UNIT ) and ( not NS.isUnitArena(unitId) ) then return end
     if ( spell.trackType == NS.TRACK_UNIT_PET ) and ( not NS.isUnitArenaPet(unitId) ) then return end
 
     -- Check if spell is disabled for current spec
