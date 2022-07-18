a9ca9cd9c856d9b273bb3562110bc6b8abcbb6d0
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2022-07-19 01:20:25 +0900

    Update UnitInfoHelpers.lua (#44)

--- UnitInfoHelpers.lua
+++ UnitInfoHelpers.lua
@@ -265,21 +265,11 @@ end
 
 NS.findArenaFrameForUnitId = function (unitId)
     if NS.isTestMode and (unitId == "player") then
-        local frame = _G["GladiusButtonFramearena1"];
-        if ( not frame ) then
-            frame = _G["sArenaEnemyFrame1"];
-        end
-
-        return frame;
+        return _G["GladiusButtonFramearena1"] or _G["sArenaEnemyFrame1"];
     else
         for i = 1, NS.MAX_ARENA_SIZE do
             if ( unitId == "arena"..i ) then
-                local frame = _G["GladiusButtonFramearena"..i];
-                if ( not frame ) then
-                    frame = _G["sArenaEnemyFrame"..i];
-                end
-
-                return frame;
+                return _G["GladiusButtonFramearena"..i] or _G["sArenaEnemyFrame"..i];
             end
         end
     end
