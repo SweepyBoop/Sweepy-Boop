7f2913ed68d52c4d62b47087445c79d4eb2684eb
SweepyBoop <bigben198891@gmail.com>
2022-12-25 00:13:03 +0900

    Update NameplateFilter.lua

--- NameplateFilter.lua
+++ NameplateFilter.lua
@@ -149,23 +149,18 @@ local function updateFrame(unitFrame, unitId)
         unitFrame.castBar:UnregisterAllEvents();
         unitFrame.castBar:Hide();
         unitFrame.BuffFrame:Hide();
-    else
-        -- Set width different from default 130, so we don't set it again
-        Plater.SetNameplateSize(unitFrame, 132, 13);
-        if ( not UnitIsPlayer(unitId) ) then
-            local npcID = unitFrame.namePlateNpcId; -- select(6, strsplit("-", UnitGUID(unitId)));
-            if ( not npcID ) or ( not showCastNpc[npcID] ) then
-                unitFrame.castBar:UnregisterAllEvents();
-                unitFrame.castBar:Hide();
-            end
+    elseif ( not UnitIsPlayer(unitId) ) then
+        local npcID = unitFrame.namePlateNpcId; -- select(6, strsplit("-", UnitGUID(unitId)));
+        if ( not npcID ) or ( not showCastNpc[npcID] ) then
+            unitFrame.castBar:UnregisterAllEvents();
+            unitFrame.castBar:Hide();
         end
     end
 end
 
 local function updateWidth(unitFrame, unitId)
     local width = unitFrame:GetSize()
-    -- Already set
-    if ( width ~= 130 ) then
+    if ( width < 50 ) then
         return
     end
 
