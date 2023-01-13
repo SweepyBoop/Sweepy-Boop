43cbe64a29f68f963c54b2a189a4f9b0ff07b8f9
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2023-01-13 11:12:30 +0900

    Class icons (#91)
    * update
    
    * flat
    
    * Update NameplateFilter.lua

--- NameplateFilter.lua
+++ NameplateFilter.lua
@@ -287,24 +287,22 @@ local ClassIconOptions = {
     }
 }
 
-local function UpdateIconFile(unitFrame, icon)
-    if UnitIsPlayer(unitFrame.unit) then
-        local class = select(2, UnitClass(unitFrame.unit))
-        if (class == unitFrame.class) then return end
+local IconPath = "Interface\\AddOns\\aSweepyBoop\\ClassIcons\\flat\\"
+local IconPathTarget = "Interface\\AddOns\\aSweepyBoop\\ClassIcons\\warcraftflat\\"
 
-        icon:SetTexture ([[Interface\TargetingFrame\UI-CLASSES-CIRCLES]])
-        icon:SetTexCoord (unpack(CLASS_ICON_TCOORDS [class]))
-        icon:SetSize (ClassIconOptions.PlayerSize, ClassIconOptions.PlayerSize)
+local function UpdateIconFile(unitFrame, icon)
+    local class = ( UnitIsPlayer(unitFrame.unit) and select(2, UnitClass(unitFrame.unit)) ) or "PET"
+    local target = UnitIsUnit("target", unitFrame.unit)
+    
+    if ( class ~= unitFrame.class ) or ( target ~= unitFrame.target ) then
+        local iconPath = ( target and IconPathTarget ) or IconPath
+        icon:SetTexture(iconPath .. class)
+        
+        local iconSize = ( class == "PET" and ClassIconOptions.PetSize ) or ClassIconOptions.PlayerSize
+        icon:SetSize(iconSize, iconSize)
 
         unitFrame.class = class
-    else
-        if (unitFrame.class == "PET") then return end
-
-        icon:SetTexture ([[Interface\Icons\inv_stbernarddogpet]])
-        icon:SetTexCoord (0, 1, 0, 1)
-        icon:SetSize (ClassIconOptions.PetSize, ClassIconOptions.PetSize)
-
-        unitFrame.class = "PET"
+        unitFrame.target = target
     end
 end
 
