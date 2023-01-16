0d988a74358336166ed06e7d959195135073be43
SweepyBoop <bigben198891@gmail.com>
2023-01-17 01:05:39 +0900

    Update LoseControl.lua

--- LoseControl.lua
+++ LoseControl.lua
@@ -1,27 +1,33 @@
 local _, NS = ...
 
-local loseControl = CreateFrame("Cooldown", nil, UIParent, "CooldownFrameTemplate")
-loseControl:SetSize(30, 30)
-loseControl:SetAllPoints()
-loseControl:RegisterEvent("LOSS_OF_CONTROL_ADDED")
-loseControl:RegisterEvent("LOSS_OF_CONTROL_UPDATE")
-loseControl:RegisterEvent("PLAYER_ENTERING_WORLD")
+local containerFrame = CreateFrame("Frame", nil, UIParent)
+containerFrame:SetSize(30, 30)
+containerFrame:SetPoint("CENTER")
+local texture = containerFrame:CreateTexture()
+texture:SetAllPoints()
 
-function loseControl:OnEvent(event, ...)
+local cooldown = CreateFrame("Cooldown", "myCooldown", containerFrame, "CooldownFrameTemplate")
+cooldown:SetAllPoints()
+
+containerFrame:RegisterEvent("LOSS_OF_CONTROL_ADDED")
+containerFrame:RegisterEvent("LOSS_OF_CONTROL_UPDATE")
+containerFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+function containerFrame:OnEvent(event, ...)
     local locData = C_LossOfControl.GetActiveLossOfControlData(1)
     if ( not locData ) then
-        loseControl:Hide()
+        containerFrame:Hide()
         return
     end
 
-    loseControl:SetTexture(locData[iconTexture])
-    loseControl:SetCooldown(locData["startTime"], locData["duration"])
+    texture:SetTexture(locData[iconTexture])
+    cooldown:SetCooldown(locData["startTime"], locData["duration"])
 
     if locData["locType"] == "ROOT" then
-        loseControl:SetAlpha(0.5)
+        containerFrame:SetAlpha(0.5)
     else
-        loseControl:SetAlpha(1)
+        containerFrame:SetAlpha(1)
     end
 
-    loseControl:Show()
+    containerFrame:Show()
 end
