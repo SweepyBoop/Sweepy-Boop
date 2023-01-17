36de5e9ef42158452cc7e3fbe4f1528c86670e96
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2023-01-17 11:03:49 +0900

    Losecontrol (#101)
    * save
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua

---
+++ LoseControl.lua
@@ -0,0 +1,36 @@
+local containerFrame = CreateFrame("Frame", nil, UIParent)
+containerFrame:SetSize(30, 30)
+containerFrame:SetPoint("CENTER")
+local texture = containerFrame:CreateTexture()
+texture:SetAllPoints()
+
+-- Assign a name so we can disable it in OmniCC
+local cooldown = CreateFrame("Cooldown", "CustomLoseControlFrame", containerFrame, "CooldownFrameTemplate")
+cooldown:SetHideCountdownNumbers(true)
+cooldown:SetAllPoints()
+
+containerFrame:RegisterEvent("LOSS_OF_CONTROL_ADDED")
+containerFrame:RegisterEvent("LOSS_OF_CONTROL_UPDATE")
+containerFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+function containerFrame:OnEvent(event, ...)
+    local locData = C_LossOfControl.GetActiveLossOfControlData(1)
+
+    if ( not locData ) or ( not locData.displayText ) or ( locData.displayType == 0 ) then
+        containerFrame:Hide()
+        return
+    end
+
+    texture:SetTexture(locData.iconTexture)
+    cooldown:SetCooldown(locData.startTime , locData.duration)
+
+    local locType = locData.locType
+    if ( locType == "ROOT" ) or ( locType == "SCHOOL_INTERRUPT" ) or ( locType == "DISARM" ) then
+        containerFrame:SetAlpha(0.5)
+    else
+        containerFrame:SetAlpha(1)
+    end
+
+    containerFrame:Show()
+end
+containerFrame:SetScript("OnEvent", containerFrame.OnEvent)
