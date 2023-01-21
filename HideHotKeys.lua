46065a94624ddb5b0f1a35ba4db7b87bd3b8cfde
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2023-01-21 16:01:32 +0900

    Hide hot keys when logging in, and slash command /shk /hhk to show/hide (#106)
    * Implement
    
    * Update HideHotKeys.lua
    
    * Update HideHotKeys.lua
    
    * Update HideHotKeys.lua
    
    * Update HideHotKeys.lua
    
    Hide on login, add slash commands
    
    * Update HideHotKeys.lua
    
    * Update HideHotKeys.lua

---
+++ HideHotKeys.lua
@@ -0,0 +1,89 @@
+local function HideHotKeys_HideBar(b, f)
+    for i = 1, 12 do
+        local o = _G[b.."Button"..i..f]
+        if (o) then
+            o:Hide()
+        end
+    end
+end
+
+local function HideHotKeys_ShowBar(b, f)
+    for i = 1, 12 do
+        local o = _G[b.."Button"..i..f]
+        if (o) then
+            if f == "HotKey" then
+                local action = _G[b.."Button"..i].action
+                local range = IsActionInRange(action)
+                if o:GetText() ~= RANGE_INDICATOR or range or range == false then
+                    o:Show()
+                end
+            else
+                o:Show()
+            end
+        end
+    end
+end
+
+local function HideHotKeys_HK_HideAll()
+    HideHotKeys_HideBar("Action", "HotKey")
+    HideHotKeys_HideBar("BonusAction", "HotKey")
+    HideHotKeys_HideBar("PetAction", "HotKey")
+    HideHotKeys_HideBar("MultiBarBottomLeft", "HotKey")
+    HideHotKeys_HideBar("MultiBarBottomRight", "HotKey")
+    HideHotKeys_HideBar("MultiBarRight", "HotKey")
+    HideHotKeys_HideBar("MultiBarLeft", "HotKey")
+end
+
+local function HideHotKeys_HK_ShowAll()
+    HideHotKeys_ShowBar("Action", "HotKey")
+    HideHotKeys_ShowBar("BonusAction", "HotKey")
+    HideHotKeys_ShowBar("PetAction", "HotKey")
+    HideHotKeys_ShowBar("MultiBarBottomLeft", "HotKey")
+    HideHotKeys_ShowBar("MultiBarBottomRight", "HotKey")
+    HideHotKeys_ShowBar("MultiBarRight", "HotKey")
+    HideHotKeys_ShowBar("MultiBarLeft", "HotKey")
+end
+
+local function HideHotKeys_MN_HideAll()
+    HideHotKeys_HideBar("Action", "Name")
+    HideHotKeys_HideBar("BonusAction", "Name")
+    HideHotKeys_HideBar("MultiBarBottomLeft", "Name")
+    HideHotKeys_HideBar("MultiBarBottomRight", "Name")
+    HideHotKeys_HideBar("MultiBarRight", "Name")
+    HideHotKeys_HideBar("MultiBarLeft", "Name")
+end
+
+
+local function HideHotKeys_MN_ShowAll()
+    HideHotKeys_ShowBar("Action", "Name")
+    HideHotKeys_ShowBar("PetAction", "Name")
+    HideHotKeys_ShowBar("BonusAction", "Name")
+    HideHotKeys_ShowBar("MultiBarBottomLeft", "Name")
+    HideHotKeys_ShowBar("MultiBarBottomRight", "Name")
+    HideHotKeys_ShowBar("MultiBarRight", "Name")
+    HideHotKeys_ShowBar("MultiBarLeft", "Name")
+end
+
+local function ShowHotKeys()
+    HideHotKeys_HK_ShowAll()
+    HideHotKeys_MN_ShowAll()
+end
+
+local function HideHotKeys()
+    HideHotKeys_HK_HideAll()
+    HideHotKeys_MN_HideAll()
+end
+
+SLASH_HIDEHOTKEYSHK1 = "/shk"
+SlashCmdList["HIDEHOTKEYSHK"] = ShowHotKeys
+
+SLASH_HIDEHOTKEYHHK1 = "/hhk"
+SlashCmdList["HIDEHOTKEYHHK"] = HideHotKeys
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function ()
+    HideHotKeys()
+end)
+
+-- TODO: hook up UPDATE_BINDINGS event and rehide keybinds based on current settings
