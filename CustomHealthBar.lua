73a07d07c26dac58da968ccc076bae18d26b6f8c
SweepyBoop <bigben198891@gmail.com>
2023-01-25 10:39:19 +0900

    Update CustomHealthBar.lua

--- CustomHealthBar.lua
+++ CustomHealthBar.lua
@@ -1,17 +1,32 @@
-local function CreateBar(previous) -- Create StatusBar with a text overlay
-    local f = CreateFrame("StatusBar", nil, UIParent)
-    f:SetSize(150, 30)
-    if not previous then
-        f:SetPoint("LEfT", 10, 0)
-    else
-        f:SetPoint("TOP", previous, "BOTTOM", 0, 10)
+local function CreateBar(index, width, height, color) -- Create StatusBar with a text overlay
+    local f = CreateFrame("StatusBar", nil, UIParent);
+    f:SetSize(width, height);
+
+    if ( index == 1 ) then
+        f:SetPoint("TOPRIGHT", PlayerFrame.portrait, "TOPLEFT", 0, 0);
+    elseif ( index == 2 ) then
+        f:SetPoint("BOTTOMRIGHT", PlayerFrame.portrait, "BOTTOMLEFT", 0, 0);
     end
-    f:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
-    f.Text = f:CreateFontString()
-    f.Text:SetFontObject(GameFontNormal)
-    f.Text:SetPoint("LEFT", 10, 0)
-    f.Text:SetJustifyH("LEFT")
-    f.Text:SetJustifyV("CENTER")
-    f:Hide() -- Hide initially
-    return f
+
+    f:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar");
+
+    local barColor = color or "green";
+    if ( barColor == "green" ) then
+        f:SetStatusBarColor(0, 1, 0);
+    elseif ( barColor == "blue" ) then
+        f:SetStatusBarColor(0, 0, 1);
+    end
+
+    f.Text = f:CreateFontString();
+    f.Text:SetFontObject(GameFontNormal);
+    f.Text:SetPoint("LEFT", 10, 0);
+    f.Text:SetJustifyH("LEFT");
+    f.Text:SetJustifyV("CENTER");
+    f.Text:SetText(index);
+    f.Text:SetTextColor(1, 1, 1);
+    f:Hide(); -- Hide initially
+    return f;
 end
+
+local pet1 = CreateBar(1, 110, 28);
+local pet2 = CreateBar(2, 110, 28);
