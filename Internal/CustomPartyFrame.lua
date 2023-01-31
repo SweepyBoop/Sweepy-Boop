381f38409ffe911253c41d04ee8dafe5ad123ac2
SweepyBoop <bigben198891@gmail.com>
2023-01-31 10:59:14 +0900

    Update CustomPartyFrame.lua

--- Internal/CustomPartyFrame.lua
+++ Internal/CustomPartyFrame.lua
@@ -1,80 +1,77 @@
 function CustomCompactPartyFrame_OnLoad(self)
-	self.applyFunc = CompactRaidGroup_ApplyFunctionToAllFrames;
-	self.isParty = true;
+    self.applyFunc = CompactRaidGroup_ApplyFunctionToAllFrames;
+    self.isParty = true;
 
-	for i=1, MEMBERS_PER_RAID_GROUP do
-		local unitFrame = _G["CustomCompactPartyFrameMember"..i];
-		unitFrame.isParty = true;
-	end
-	
-	CustomCompactPartyFrame_RefreshMembers();
-	
-	self.title:SetText("Custom " .. PARTY);
-	self.title:Disable();
+    for i=1, MEMBERS_PER_RAID_GROUP do
+        local unitFrame = _G["CustomCompactPartyFrameMember"..i];
+        unitFrame.isParty = true;
+    end
+    
+    CustomCompactPartyFrame_RefreshMembers();
+    
+    self.title:SetText("Custom " .. PARTY);
+    self.title:Disable();
 end
 
 hooksecurefunc("CompactPartyFrame_UpdateVisibility", function ()
-	if not CustomCompactPartyFrame then
-		return;
-	end
-	
-	local isInArena = IsActiveBattlefieldArena();
-	local groupFramesShown = (IsInGroup() and (isInArena or not IsInRaid())) or EditModeManagerFrame:ArePartyFramesForcedShown();
-	local showCompactPartyFrame = groupFramesShown and EditModeManagerFrame:UseRaidStylePartyFrames();
-	CompactPartyFrame:SetShown(not showCompactPartyFrame);
-	CustomCompactPartyFrame:SetShown(showCompactPartyFrame);
+    if ( not CompactPartyFrame ) or ( not CustomCompactPartyFrame ) then
+        return;
+    end
+    
+    local isInArena = IsActiveBattlefieldArena();
+    local groupFramesShown = (IsInGroup() and (isInArena or not IsInRaid())) or EditModeManagerFrame:ArePartyFramesForcedShown();
+    local showCompactPartyFrame = groupFramesShown and EditModeManagerFrame:UseRaidStylePartyFrames();
+    CompactPartyFrame:SetShown(false);
+    CustomCompactPartyFrame:SetShown(showCompactPartyFrame);
 end)
 
 function CustomCompactPartyFrame_RefreshMembers()
-	if not CustomCompactPartyFrame then
-		return;
-	end
+    if not CustomCompactPartyFrame then
+        return;
+    end
 
-	local units = {};
+    local units = {};
 
-	table.insert(units, "party1");
+    table.insert(units, "party1");
     table.insert(units, "player");
     for i=3, MEMBERS_PER_RAID_GROUP do
         table.insert(units, "party"..(i-1));
     end
 
-	for index, realPartyMemberToken in ipairs(units) do
-		local unitFrame = _G["CustomCompactPartyFrameMember"..index];
+    for index, realPartyMemberToken in ipairs(units) do
+        local unitFrame = _G["CustomCompactPartyFrameMember"..index];
 
-		local usePlayerOverride = EditModeManagerFrame:ArePartyFramesForcedShown() and not UnitExists(realPartyMemberToken);
-		local unitToken = usePlayerOverride and "player" or realPartyMemberToken;
+        local usePlayerOverride = EditModeManagerFrame:ArePartyFramesForcedShown() and not UnitExists(realPartyMemberToken);
+        local unitToken = usePlayerOverride and "player" or realPartyMemberToken;
 
-		CompactUnitFrame_SetUnit(unitFrame, unitToken);
-		CompactUnitFrame_SetUpFrame(unitFrame, DefaultCompactUnitFrameSetup);
-		CompactUnitFrame_SetUpdateAllEvent(unitFrame, "GROUP_ROSTER_UPDATE");
-	end
+        CompactUnitFrame_SetUnit(unitFrame, unitToken);
+        CompactUnitFrame_SetUpFrame(unitFrame, DefaultCompactUnitFrameSetup);
+        CompactUnitFrame_SetUpdateAllEvent(unitFrame, "GROUP_ROSTER_UPDATE");
+    end
 
-	CompactRaidGroup_UpdateBorder(CustomCompactPartyFrame);
+    CompactRaidGroup_UpdateBorder(CustomCompactPartyFrame);
 end
 
 hooksecurefunc("CompactPartyFrame_SetFlowSortFunction", function()
-	if not CustomCompactPartyFrame then
-		return;
-	end
-	CustomCompactPartyFrame_RefreshMembers();
+    if not CustomCompactPartyFrame then
+        return;
+    end
+    CustomCompactPartyFrame_RefreshMembers();
 end)
 
 local function Custom_CompactPartyFrame_Generate()
-	local frame = CustomCompactPartyFrame;
-	local didCreate = false;
-	if not frame then
-		frame = CreateFrame("Frame", "CustomCompactPartyFrame", PartyFrame, "CompactPartyFrameTemplate");
-		CompactRaidGroup_UpdateBorder(frame);
-		frame:RegisterEvent("GROUP_ROSTER_UPDATE");
-		didCreate = true;
-	end
-	return frame, didCreate;
+    local frame = CustomCompactPartyFrame;
+    if not frame then
+        frame = CreateFrame("Frame", "CustomCompactPartyFrame", PartyFrame, "CompactPartyFrameTemplate");
+        CompactRaidGroup_UpdateBorder(frame);
+        frame:RegisterEvent("GROUP_ROSTER_UPDATE");
+    end
 end
 
 hooksecurefunc("CompactPartyFrame_Generate", function ()
-	return Custom_CompactPartyFrame_Generate();
+    Custom_CompactPartyFrame_Generate();
 end)
 
 if ( not CustomCompactPartyFrame ) then
-	Custom_CompactPartyFrame_Generate();
+    Custom_CompactPartyFrame_Generate();
 end
