ec1537be741d0aa4b342ebf41dbcaa7938ba7c23
SweepyBoop <bigben198891@gmail.com>
2023-02-04 11:53:28 +0900

    save

--- Internal/RaidFrameDebuffs.lua
+++ Internal/RaidFrameDebuffs.lua
@@ -5,6 +5,7 @@ local topRightAura = {}; -- Show a warning aura to the top right of a raid frame
 
 local centerAuraSpells = {
     102352, -- Cenarion Ward
+    774, -- Test (Rejuv)
 };
 
 local topRightSpells = {
@@ -17,25 +18,38 @@ local topRightSpells = {
 local function SetupRaidFrame(frame)
     if ( not centerAura[frame] ) then
         local centerSize = 25;
-        centerAura[frame] = CreateFrame("Frame", nil, frame, "CompactBuffTemplate");
+        centerAura[frame] = CreateFrame("Frame", nil, frame, "CustomCompactAuraTemplate");
         centerAura[frame]:SetSize(centerSize, centerSize);
         centerAura[frame]:SetPoint("BOTTOM", frame, "CENTER");
     end
 
-    if ( not topRightAura[frame] ) then
+    --[[ if ( not topRightAura[frame] ) then
         local topRightSize = 27;
         topRightAura[frame] = CreateFrame("Frame", nil, frame, "CompactBuffTemplate");
         topRightAura[frame]:SetSize(topRightSize, topRightSize);
         topRightAura[frame]:SetPoint("TOPLEFT", frame, "TOPRIGHT");
-    end
+    end ]]
 
     return centerAura[frame], topRightAura[frame];
 end
 
 local function UpdateRaidFrame(frame)
+    if ( frame:GetParent() ~= CompactPartyFrame ) then return end
+
     local center, topRight = SetupRaidFrame(frame);
 
-    
+    local centerSet;
+    for _, spell in ipairs(centerAuraSpells) do
+        local duration, expirationTime = NS.Util_GetUnitBuff(frame.displayedUnit, spell);
+        if duration then
+            center.icon:SetCooldown(expirationTime - duration, duration);
+            centerSet = true;
+            break;
+        end
+    end
+    if ( not centerSet ) then
+        center.icon:SetCooldown(0, 0);
+    end
 end
 
 hooksecurefunc("CompactUnitFrame_UpdateAuras", function (frame)
