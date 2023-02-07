f773f25d2c15c7f74e19e95b07ed3407497e90c3
SweepyBoop <bigben198891@gmail.com>
2023-02-07 17:00:33 +0900

    Update CooldownTracking.lua

--- CooldownTracking/CooldownTracking.lua
+++ CooldownTracking/CooldownTracking.lua
@@ -1,2 +1,30 @@
 local _, NS = ...;
 
+local cooldowns = NS.cooldownSpells;
+local resets = NS.cooldownResets;
+
+for spellID, spell in pairs(cooldowns) do
+    spell.priority = spell.index or 100;
+
+    if ( not spell.class ) then
+        print("Spell missing class:", spellID);
+    end
+
+    -- Validate class
+    if spell.class and ( type(spell.class) ~= "string" ) then
+        print("Invalid class for spellID:", spellID);
+    end
+end
+
+local growCenter = {
+    diretion = "CENTER",
+    anchor = "CENTER",
+    margin = 3,
+};
+
+local growRight = {
+    direction = "RIGHT",
+    anchor = "LEFT",
+    margin = 3,
+};
+
