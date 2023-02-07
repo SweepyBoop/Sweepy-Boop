393a0c121752b2f42ff30f0556f2f4728104a09d
SweepyBoop <bigben198891@gmail.com>
2023-02-07 21:30:39 +0900

    Update IconGroupUtil.lua

--- IconGroupUtil.lua
+++ IconGroupUtil.lua
@@ -109,6 +109,8 @@ NS.IconGroup_Remove = function (group, icon)
     end
 end
 
+-- For arena offensive cooldown tracking, index is just spellID since we are always tracking a single unitId
+-- For Omnibar icons, we will use unitId - spellID
 NS.IconGroup_PopulateIcon = function (group, icon, index)
     icon:SetParent(group);
     group.icons[index] = icon;
