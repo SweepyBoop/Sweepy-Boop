d935af686b2966ab5773712d475e7ab2ce739080
SweepyBoop <bigben198891@gmail.com>
2023-02-22 09:52:47 +0900

    Update FixBlizzardRaidAggro.lua

--- FixBlizzardRaidAggro.lua
+++ FixBlizzardRaidAggro.lua
@@ -8,6 +8,9 @@ local UnitGUID = UnitGUID;
 local IsActiveBattlefieldArena = IsActiveBattlefieldArena;
 local GetThreatStatusColor = GetThreatStatusColor;
 local CreateFrame = CreateFrame;
+local hooksecurefunc = hooksecurefunc;
+local UnitIsUnit = UnitIsUnit;
+local CompactPartyFrame = CompactPartyFrame;
 
 -- IMPORTANT!!!
 -- Make sure you disable Interface -> Raid Profiles -> "Display Aggro Highlight", and do a /reload
@@ -16,103 +19,69 @@ local CreateFrame = CreateFrame;
 -- Test mode: target a raid frame and check if the aggro highlight is showing up
 local isTestMode = NS.isTestMode;
 
-local arenaRoles = {}
-
-local function ShouldClearAggro(event)
-    return (event == NS.PLAYER_ENTERING_WORLD) or (event == NS.ARENA_PREP_OPPONENT_SPECIALIZATIONS)
-end
+local arenaRoles = {};
 
 -- Make sure to only pass "player", "arena".. 1~3
 local function GetArenaRole(unitId)
     if ( not arenaRoles[unitId] ) then
         if ( unitId == "player" ) then
-            local currentSpec = GetSpecialization()
-            arenaRoles[unitId] = select(5, GetSpecializationInfo(currentSpec))
+            local currentSpec = GetSpecialization();
+            arenaRoles[unitId] = select(5, GetSpecializationInfo(currentSpec));
         else
-            local arenaIndex = string.sub(unitId, -1, -1)
-            local specID = GetArenaOpponentSpec(arenaIndex)
+            local arenaIndex = string.sub(unitId, -1, -1);
+            local specID = GetArenaOpponentSpec(arenaIndex);
             if specID then
-                arenaRoles[unitId] = select(5, GetSpecializationInfoByID(specID))
+                arenaRoles[unitId] = select(5, GetSpecializationInfoByID(specID));
             end
         end
     end
 
-    return arenaRoles[unitId]
+    return arenaRoles[unitId];
 end
 
-local function CalculateAggro()
-    local aggro = {}
+local refreshFrame = CreateFrame("Frame");
+refreshFrame:RegisterEvent(NS.PLAYER_ENTERING_WORLD);
+refreshFrame:RegisterEvent(NS.ARENA_PREP_OPPONENT_SPECIALIZATIONS);
+refreshFrame:SetScript("OnEvent", function ()
+    arenaRoles = {};
+end)
+
+local function ShouldShowAggro(unit)
+    local arena = IsActiveBattlefieldArena();
+    if ( not arena ) and ( not isTestMode ) then
+        return false;
+    end
 
     if isTestMode then
-        if GetArenaRole("player") ~= "DAMAGER" then return end
-        local guid = UnitGUID("playertarget")
-        if guid then
-            aggro[guid] = true
+        if ( GetArenaRole("player") == "DAMAGER" ) then
+            return UnitIsUnit(unit, "target");
         end
     else
         for i = 1, NS.MAX_ARENA_SIZE do
-            if GetArenaRole("arena" .. i) == "DAMAGER" then
-                local guidTarget = UnitGUID("arena" .. i .. "target")
-                if guidTarget then
-                    aggro[guidTarget] = true
-                end
-            end
-        end
-    end
-
-    return aggro
-end
-
-local function EventHandler(self, event, unitTarget)
-    if ShouldClearAggro(event) then
-        -- Upon entering a new zone, clear the aggro highlight
-        arenaRoles = {}
-
-        for i = 1, NS.MAX_PARTY_SIZE do
-            local frame = _G["CompactPartyFrameMember"..i]
-            -- Check if the user has Blizzard default highlight on
-            if frame and frame.optionTable.displayAggroHighlight then return end
-
-            if frame and frame.aggroHighlight:IsShown() then
-                frame.aggroHighlight:Hide()
-            end
-        end
-    elseif (event == "UNIT_TARGET") and unitTarget then
-        -- Only enable highlight inside an arena
-        if (not IsActiveBattlefieldArena()) and ( not isTestMode ) then return end
-
-        local aggro = CalculateAggro();
-
-        for i = 1, NS.MAX_PARTY_SIZE do
-            local frame = _G["CompactPartyFrameMember"..i]
-            -- Check if the user has Blizzard default highlight on
-            if frame and frame.optionTable.displayAggroHighlight then return end
-
-            if frame then
-                local guid = UnitGUID(frame.unit)
-
-                -- 1: 1.00, 1.00, 0.47 (yellow)
-                -- 2: 1.00, 0.60, 0.00 (orange)
-                -- 3: 1.00, 0.00, 0.00 (red)
-                if aggro and guid and aggro[guid] then
-                    frame.aggroHighlight:SetVertexColor(GetThreatStatusColor(3)) -- red
-                    frame.aggroHighlight:Show()
-                else
-                    frame.aggroHighlight:Hide()
-                end
+            if UnitIsUnit(unit, "arena" .. i .. "target") and ( GetArenaRole("arena" .. i) == "DAMAGER" ) then
+                return true;
             end
         end
     end
 end
-
-local frame;
 
 function SweepyBoop:SetupRaidFrameAggroHighlight()
     if self.db.profile.raidFrameAggroHighlightEnabled then
-        frame = CreateFrame("Frame")
-        frame:RegisterEvent(NS.PLAYER_ENTERING_WORLD)
-        frame:RegisterEvent(NS.ARENA_PREP_OPPONENT_SPECIALIZATIONS)
-        frame:RegisterEvent(NS.UNIT_TARGET)
-        frame:SetScript("OnEvent", EventHandler)
+        hooksecurefunc("CompactUnitFrame_UpdateName", function (frame)
+            if frame:IsForbidden() then
+                return;
+            end
+
+            if ( frame:GetParent() ~= CompactPartyFrame ) then
+                return;
+            end
+
+            if ShouldShowAggro(frame.unit) then
+                frame.aggroHighlight:SetVertexColor(GetThreatStatusColor(3)); -- red
+                frame.aggroHighlight:Show();
+            else
+                frame.aggroHighlight:Hide();
+            end
+        end)
     end
 end
