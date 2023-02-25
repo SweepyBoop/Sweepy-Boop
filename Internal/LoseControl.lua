24f72d28b0c488ba4c03ff3268918b4d68735063
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2023-02-25 22:48:47 +0900

    Fix the issue where class icon appears on top of party nameplates during duel (#174)
    * Update ClassAndPetIcons.lua
    
    * Update LoseControl.lua
    
    * Update LoseControl.lua

--- Internal/LoseControl.lua
+++ Internal/LoseControl.lua
@@ -8,15 +8,15 @@ local SendChatMessage = SendChatMessage;
 local containerFrame = CreateFrame("Frame", nil, UIParent)
 containerFrame:SetSize(30, 30)
 containerFrame:SetPoint("CENTER")
-local texture = containerFrame:CreateTexture()
-texture:SetAllPoints()
+containerFrame.texture = containerFrame:CreateTexture()
+containerFrame.texture:SetAllPoints()
 
 -- Assign a name so we can disable it in OmniCC
 -- https://github.com/tomrus88/BlizzardInterfaceCode/blob/master/Interface/FrameXML/LossOfControlFrame.xml
-local cooldown = CreateFrame("Cooldown", NS.HIDETIMEROMNICC .. "LoseControl", containerFrame, "CooldownFrameTemplate")
-cooldown:SetHideCountdownNumbers(true)
-cooldown:SetDrawEdge(true);
-cooldown:SetAllPoints()
+containerFrame.cooldown = CreateFrame("Cooldown", NS.HIDETIMEROMNICC .. "LoseControl", containerFrame, "CooldownFrameTemplate")
+containerFrame.cooldown:SetHideCountdownNumbers(true)
+containerFrame.cooldown:SetDrawEdge(true);
+containerFrame.cooldown:SetAllPoints()
 
 containerFrame:RegisterEvent(NS.LOSS_OF_CONTROL_ADDED)
 containerFrame:RegisterEvent(NS.LOSS_OF_CONTROL_UPDATE)
@@ -26,24 +26,24 @@ function containerFrame:OnEvent(event, ...)
     local locData = C_LossOfControl.GetActiveLossOfControlData(1)
 
     if ( not locData ) or ( not locData.displayText ) or ( locData.displayType == 0 ) then
-        containerFrame:Hide()
+        self:Hide()
         return
     end
 
-    texture:SetTexture(locData.iconTexture)
+    self.texture:SetTexture(locData.iconTexture)
 
     if locData.duration then -- Some auras have no duration, such as solar beam
-        cooldown:SetCooldown(locData.startTime , locData.duration)
+        self.cooldown:SetCooldown(locData.startTime , locData.duration)
     end
 
     local locType = locData.locType
     if ( locType == "ROOT" ) or ( locType == "SCHOOL_INTERRUPT" ) or ( locType == "DISARM" ) then
-        containerFrame:SetAlpha(0.5)
+        self:SetAlpha(0.5)
     else
-        containerFrame:SetAlpha(1)
+        self:SetAlpha(1)
     end
 
-    containerFrame:Show()
+    self:Show()
 
     -- Send notification to group
     if IsActiveBattlefieldArena() and ( event == NS.LOSS_OF_CONTROL_ADDED ) then
