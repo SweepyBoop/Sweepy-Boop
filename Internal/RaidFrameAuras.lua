6fc21e73ba21e64598a09bb340b2ad630a73723c
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2023-03-07 12:34:22 +0900

    Raid auras (#180)
    * Update RaidFrameAuras.lua
    
    * Update RaidFrameAuras.lua
    
    * Update RaidFrameAuras.lua
    
    * Update RaidFrameAuras.lua
    
    * Update RaidFrameAuras.lua

--- Internal/RaidFrameAuras.lua
+++ Internal/RaidFrameAuras.lua
@@ -5,9 +5,10 @@ local CompactPartyFrame = CompactPartyFrame;
 local hooksecurefunc = hooksecurefunc;
 local UnitIsPlayer = UnitIsPlayer;
 
-local test = false;
+local test = NS.isTestMode;
 
 local centerAura = {}; -- Show an important aura at the center of a raid frame
+local topRightAura = {};
 
 -- topRightAura removed since those debuffs are covered by BigDebuffs
 -- Hemotoxin is combined into Shiv debuff
@@ -22,14 +23,75 @@ if test then
     table.insert(centerAuraSpells, 774); -- Rejuv (test)
 end
 
-local function SetAuraFrame(frame)
-    local size = 22;
+local topRightAuraSpells = {
+    48792, -- Icebound Fortitude
+    51052, -- Anti-Magic Zone
+    48707, -- Anti-Magic Shell
+
+    196555, -- Netherwalk
+    196718, -- Darkness
+    198589, -- Blur
+
+    102342, -- Ironbark
+    61336, -- Survival Instincts
+
+    363916, -- Obsidian Scales
+
+    186265, -- Aspect of the Turtle
+    53480, -- Roar of Sacrifice
+
+    45438, -- Ice Block
+    342245, -- Alter Time
+
+    122470, -- Touch of Karma
+    116849, -- Life Cacoon
+
+    642, -- Divine Shield
+    1022, -- Blessing of Protection
+    184662, -- Shield of Vengeance
+
+    47788, -- Guardian Spirit
+    33206, -- Pain Suppression
+    47585, -- Dispersion
+
+    31224, -- Cloak of Shadows
+    5277, -- Evasion
+    1856, -- Vanish
+
+    108271, -- Astral Shift
+    210918, -- Ethereal Form
+
+    108416, -- Dark Pact
+    104773, -- Unending Resolve
+    212295, -- Nether Ward
+
+    118038, -- Die by the Sword
+    184364, -- Enraged Regeneration
+    97462, -- Rallying Cry
+
+    345231, -- Gladiator's Emblem
+};
+if test then
+    table.insert(topRightAuraSpells, 8936); -- Regrowth (test)
+    table.insert(topRightAuraSpells, 774); -- Rejuv (test)
+    table.insert(topRightAuraSpells, 194384);  -- Atonement
+end
+
+local function SetAuraFrame(frame, buffSize, overlayGlow)
+    local size = buffSize or 22;
     frame:SetSize(size, size);
     frame.cooldown:SetDrawEdge(false);
     frame.cooldown:SetAlpha(1);
     frame.cooldown:SetDrawBling(false);
     frame.cooldown:SetDrawSwipe(true);
     frame.cooldown:SetReverse(true);
+
+    if overlayGlow then
+        frame.spellActivationAlert = CreateFrame("Frame", nil, frame, "ActionBarButtonSpellActivationAlert");
+        frame.spellActivationAlert:SetSize(size * 1.4, size * 1.4);
+        frame.spellActivationAlert:SetPoint("CENTER", frame, "CENTER", 0, 0);
+        frame.spellActivationAlert:Hide();
+    end
 end
 
 local function SetupRaidFrame(frame)
@@ -40,16 +102,24 @@ local function SetupRaidFrame(frame)
         centerAura[frame]:Hide();
     end
 
-    return centerAura[frame];
+    if ( not topRightAura[frame] ) then
+        topRightAura[frame] = CreateFrame("Frame", nil, frame, "CustomCompactAuraTemplate");
+        SetAuraFrame(topRightAura[frame], 25, true);
+        topRightAura[frame]:SetPoint("TOPLEFT", frame, "TOPRIGHT");
+        topRightAura[frame]:Hide();
+    end
+
+    return centerAura[frame], topRightAura[frame];
 end
 
 local function UpdateRaidFrame(frame)
     if ( frame:GetParent() ~= CompactPartyFrame ) then return end
 
-    local center = SetupRaidFrame(frame);
+    local center, topRight = SetupRaidFrame(frame);
 
     local centerSet;
-    for _, spell in ipairs(centerAuraSpells) do
+    for i = 1, #(centerAuraSpells) do
+        local spell = centerAuraSpells[i];
         local name, icon, _, _, duration, expirationTime, source = NS.Util_GetUnitBuff(frame.displayedUnit, spell);
         if duration and ( source == "player" ) then
             center.icon:SetTexture(icon);
@@ -62,6 +132,25 @@ local function UpdateRaidFrame(frame)
 
     if ( not centerSet ) then
         center:Hide();
+    end
+
+    local topRightSet;
+    for i = 1, #(topRightAuraSpells) do
+        local spell = topRightAuraSpells[i];
+        local name, icon, _, _, duration, expirationTime, source = NS.Util_GetUnitBuff(frame.displayedUnit, spell);
+        if duration then
+            topRight.icon:SetTexture(icon);
+            topRight.cooldown:SetCooldown(expirationTime - duration, duration);
+            NS.ShowOverlayGlow(topRight);
+            topRight:Show();
+            topRightSet = true;
+            break;
+        end
+    end
+
+    if ( not topRightSet ) then
+        NS.HideOverlayGlow(topRight);
+        topRight:Hide();
     end
 end
 
