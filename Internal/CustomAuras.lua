c1cbb8e8ea96d5ccf18b01afc0518ad3aaf739c8
SweepyBoop <bigben198891@gmail.com>
2023-03-19 21:51:56 +0900

    Update CustomAuras.lua

--- Internal/CustomAuras.lua
+++ Internal/CustomAuras.lua
@@ -345,7 +345,7 @@ local testGlowingBuffIcon = false;
 -- The first ActionBarButtonSpellActivationAlert created seems to be corrupted by other icons, so we create a dummy here that does nothing
 local dummy = CreateFrame("Frame", nil, UIParent, "ActionBarButtonSpellActivationAlert");
 
-local precongnition = CreateGlowingBuffIcon(377360, 35, "CENTER", UIParent, "CENTER", 0, 60);
+local precongnition = CreateGlowingBuffIcon(377362, 35, "CENTER", UIParent, "CENTER", 0, 60);
 
 if ( class == NS.DRUID ) then
     local wildSynthesis = CreateStackBuffIcon(400534, 36, "BOTTOM", _G["ActionButton10"], "TOP", 0, 50, 3);
