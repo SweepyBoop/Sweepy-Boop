66d79708a7242a732d5851cd98a2831af85365fb
SweepyBoop <bigben198891@gmail.com>
2023-04-05 11:15:00 +0900

    Update DampenDisplay.lua

--- Archive/DampenDisplay.lua
+++ Archive/DampenDisplay.lua
@@ -1,30 +1,40 @@
+local _, NS = ...;
+
+local CreateFrame = CreateFrame;
+local UIParent = UIParent;
+local UIWidgetTopCenterContainerFrame = UIWidgetTopCenterContainerFrame;
+local GameFontNormalSmall = GameFontNormalSmall;
+local GetSpellInfo = GetSpellInfo;
+local C_Commentator = C_Commentator;
+local IsInInstance = IsInInstance;
+
 -- Show dampen %
-local dampeningFrame = CreateFrame('Frame', nil , UIParent)
-dampeningFrame:SetSize(200, 12)
-dampeningFrame:SetPoint('TOP', UIWidgetTopCenterContainerFrame, 'BOTTOM', 0, -2)
-dampeningFrame.text = dampeningFrame:CreateFontString(nil, 'BACKGROUND')
-dampeningFrame.text:SetFontObject(GameFontNormalSmall)
-dampeningFrame.text:SetAllPoints()
-dampeningFrame.timeSinceLastUpdate = 0
-local updateInterval = 5
-local dampeningText = GetSpellInfo(110310)
+local dampeningFrame = CreateFrame('Frame', nil , UIParent);
+dampeningFrame:SetSize(200, 12);
+dampeningFrame:SetPoint('TOP', UIWidgetTopCenterContainerFrame, 'BOTTOM', 0, -2);
+dampeningFrame.text = dampeningFrame:CreateFontString(nil, 'BACKGROUND');
+dampeningFrame.text:SetFontObject(GameFontNormalSmall);
+dampeningFrame.text:SetAllPoints();
+dampeningFrame.timeSinceLastUpdate = 0;
+local updateInterval = 5;
+local dampeningText = GetSpellInfo(110310);
 dampeningFrame:SetScript('OnUpdate', function(self, elapsed)
-    self.timeSinceLastUpdate = self.timeSinceLastUpdate + elapsed
+    self.timeSinceLastUpdate = self.timeSinceLastUpdate + elapsed;
 
     if self.timeSinceLastUpdate > updateInterval then
-        self.text:SetText(dampeningText..': '..C_Commentator.GetDampeningPercent()..'%')
-        self.timeSinceLastUpdate = 0
+        self.text:SetText(dampeningText..': ' .. C_Commentator.GetDampeningPercent() .. '%');
+        self.timeSinceLastUpdate = 0;
     end
 end)
 
 local function ShowDampening(self, event)
-    local _, instanceType = IsInInstance()
+    local _, instanceType = IsInInstance();
     if instanceType == "arena" then
-        dampeningFrame:Show()
+        dampeningFrame:Show();
     else
-        dampeningFrame:Hide()
+        dampeningFrame:Hide();
     end
 end
-local dampen = CreateFrame('Frame', 'showDampening')
-dampen:RegisterEvent("PLAYER_ENTERING_WORLD")
-dampen:SetScript('OnEvent', ShowDampening)
+local dampen = CreateFrame('Frame', 'showDampening');
+dampen:RegisterEvent("PLAYER_ENTERING_WORLD");
+dampen:SetScript('OnEvent', ShowDampening);
