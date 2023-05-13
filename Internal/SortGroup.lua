32390329c54cbd94ddcf5ef1dd9282239ef96710
SweepyBoop <bigben198891@gmail.com>
2023-05-13 11:20:53 +0900

    Block updates when entering combat

--- Internal/SortGroup.lua
+++ Internal/SortGroup.lua
@@ -13,7 +13,9 @@ local InCombatLockdown = InCombatLockdown;
 local C_Timer = C_Timer;
 local CompactPartyFrame = CompactPartyFrame;
 local CompactPartyFrameTitle = CompactPartyFrameTitle;
+local CompactRaidFrameContainer = CompactRaidFrameContainer;
 local hooksecurefunc = hooksecurefunc;
+local CreateFrame = CreateFrame;
 
 local function Compare(left, right)
     local leftToken, rightToken = left.unit, right.unit;
@@ -78,3 +80,40 @@ hooksecurefunc("CompactRaidGroup_UpdateLayout", function (frame)
         TrySort();
     end
 end)
+
+local function PauseUpdates()
+    if CompactRaidFrameContainer and not CompactRaidFrameContainer:UnregisterEvent("GROUP_ROSTER_UPDATE") then
+        NS:Warning("Failed to unregister event GROUP_ROSTER_UPDATE from CompactRaidFrameContainer.")
+    end
+
+    if CompactPartyFrame and not CompactPartyFrame:UnregisterEvent("GROUP_ROSTER_UPDATE") then
+        NS:Warning("Failed to register event GROUP_ROSTER_UPDATE from CompactPartyFrame.")
+    end
+end
+
+local function ResumeUpdates()
+    if CompactRaidFrameContainer and not CompactRaidFrameContainer:RegisterEvent("GROUP_ROSTER_UPDATE") then
+        NS:Warning("Failed to register event GROUP_ROSTER_UPDATE to CompactRaidFrameContainer.")
+    end
+
+    if CompactPartyFrame and not CompactPartyFrame:RegisterEvent("GROUP_ROSTER_UPDATE") then
+        NS:Warning("Failed to register event GROUP_ROSTER_UPDATE to CompactPartyFrame.")
+    end
+end
+
+local function OnEvent(_, event)
+    if event == "PLAYER_REGEN_ENABLED" then
+        ResumeUpdates();
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        PauseUpdates();
+    elseif "GROUP_ROSTER_UPDATE" and InCombatLockdown() then
+        NS:Debug("Blocked raid frame update during combat.")
+    end
+end
+
+-- Combat blocking: pause updates when entering combat, and resume when leaving combat
+local combatBlockingFrame = CreateFrame("Frame");
+combatBlockingFrame:HookScript("OnEvent", OnEvent);
+combatBlockingFrame:RegisterEvent("PLAYER_REGEN_ENABLED");
+combatBlockingFrame:RegisterEvent("PLAYER_REGEN_DISABLED");
+combatBlockingFrame:RegisterEvent("GROUP_ROSTER_UPDATE");
