235bb9bd8950c6db5608a337399f2acacd66df69
SweepyBoop <bigben198891@gmail.com>
2023-05-14 18:05:48 +0900

    Update LoseControl.lua

--- Internal/LoseControl.lua
+++ Internal/LoseControl.lua
@@ -52,8 +52,8 @@ function containerFrame:OnEvent(event, ...)
     -- Send notification to group
     if IsActiveBattlefieldArena() and ( event == NS.LOSS_OF_CONTROL_ADDED ) and ( UnitGroupRolesAssigned("player") == "HEALER" ) then
         local now = GetTime();
-        if ( now < self.lastMsgSent + 1.5 ) then
-            -- Don't send more than one messages within 1.5 sec.
+        if ( now < self.lastMsgSent + 2 ) then
+            -- Don't send more than one messages within 2 sec.
             return;
         end
 
