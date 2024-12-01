fbc6af98832d76d51fdf533df1e0b1fd67ac3729
Kun Liu <kunl@microsoft.com>
2024-11-30 18:04:27 -0800

    update

--- AddonUpdate.cmd
+++ AddonUpdate.cmd
@@ -1,8 +1,8 @@
-set "workDir=%~dp0"
-set "addonDir=D:\World of Warcraft\_retail_\Interface\Addons\SweepyBoop"
+set workDir=%~dp0
+set addonDir="D:\World of Warcraft\_retail_\Interface\Addons\SweepyBoop"
 
 rmdir /s /q %addonDir%
 mkdir %addonDir%
-xcopy "%workDir%" "%addonDir%" /E /I /Y
+xcopy %workDir% %addonDir% /E /I /Y
 
-echo NS.internal = true;>> "%addonDir%\Constants.lua"
+echo NS.internal = true;>> %addonDir%\Constants.lua
