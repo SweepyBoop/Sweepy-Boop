b03e838579d81f24bee831d49499ec80ed52a71a
Kun Liu <kunl@microsoft.com>
2024-12-05 12:08:37 -0800

    update

--- ArenaSurrender.lua
+++ ArenaSurrender.lua
@@ -4,6 +4,8 @@ SlashCmdList["CHAT_AFK"] = function(msg)
 	if IsActiveBattlefieldArena() and SweepyBoop.db.profile.arenaSurrenderEnabled then
 		if CanSurrenderArena() then
 			SurrenderArena();
+		elseif SweepyBoop.db.profile.skipLeaveArenaConfirmation then
+			LeaveBattlefield();
 		else
 			ConfirmOrLeaveBattlefield();
 		end
