2c53b9d7a3eaef86296c7b47a523b600c8878dcc
Kun Liu <kunl@microsoft.com>
2024-12-07 15:22:24 -0800

    update script

---
+++ AddonUpdate.ps1
@@ -0,0 +1,12 @@
+$workDir = $PSScriptRoot
+$addonDir = "D:\World of Warcraft\_retail_\Interface\Addons\SweepyBoop"
+
+if (Test-Path -Path $addonDir) {
+    Remove-Item -Path $addonDir -Recurse -Force
+}
+New-Item -ItemType Directory -Path $addonDir | Out-Null
+
+Copy-Item -Path "$workDir\*" -Destination $addonDir -Recurse -Force
+
+$constantsFile = Join-Path -Path $addonDir -ChildPath "Constants.lua"
+"addon.internal = true;" | Out-File -FilePath $constantsFile -Append -Encoding UTF8
