5ee663208b8c1bd0dbb14f7586ac88ee196bdd0d
Kun Liu <kunl@microsoft.com>
2024-12-16 11:51:12 -0800

    update warlock

--- Common/ArenaEnemyUtilitySpells.lua
+++ Common/ArenaEnemyUtilitySpells.lua
@@ -400,7 +400,9 @@ addon.utilitySpells = {
     [89766] = {cooldown = 30, class = addon.WARLOCK, trackPet = true, category = SPELLCATEGORY.CROWDCONTROL}, -- Axe Toss
         [119914] = { parent = 89766 }, -- Axe Toss (Command Demon Ability) (only shows up once no matter which version is cast)
     -- Defensive
-    [104773] = {cooldown = 180, class = addon.WARLOCK, opt_lower_cooldown = 135, category = SPELLCATEGORY.DEFENSIVE}, -- Unending Resolve
+    [104773] = {cooldown = 180, class = addon.WARLOCK, opt_lower_cooldown = 135, category = SPELLCATEGORY.DEFENSIVE, index = addon.SPELLPRIORITY.HIGH}, -- Unending Resolve
+    [410598] = {cooldown = 60, class = addon.WARLOCK, opt_lower_cooldown = 135, category = SPELLCATEGORY.DEFENSIVE}, -- Soul Rip
+    [108416] = {cooldown = 45, class = addon.WARLOCK, opt_lower_cooldown = 135, category = SPELLCATEGORY.DEFENSIVE}, -- Dark Pact
     -- Interrupt
     [19647] = {cooldown = 24, class = addon.WARLOCK, trackPet = true, category = SPELLCATEGORY.INTERRUPT}, -- Spell Lock (Command Demon Ability)
         --[119910] = {parent = 19647}, -- Spell Lock (Felhunter)
