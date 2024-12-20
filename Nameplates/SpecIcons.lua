df628b9f94421c47bdca39f3bfa34a875e6e4063
Kun Liu <kunl@microsoft.com>
2024-12-20 12:44:59 -0800

    save

--- Nameplates/SpecIcons.lua
+++ Nameplates/SpecIcons.lua
@@ -1,7 +1,14 @@
 local _, addon = ...;
-local iconSize = 25;
+local iconSize = 24;
+
+local healerIconID = "interface/lfgframe/uilfgprompts";
 
 local function ShouldShowSpecIcon(unitId) -- Return icon ID if should show, otherwise nil
+    if addon.isTestMode then
+        return ( UnitIsUnit(unitId, "target") and addon.specIconHealer ) or (UnitIsUnit(unitId, "focus") and healerIconID );
+    end
+
+    local config = SweepyBoop.db.profile.nameplatesEnemy;
     for i = 1, addon.MAX_ARENA_SIZE do
         if UnitIsUnit(unitId, "arena" .. i) then
             -- i is the opponent index
@@ -9,9 +16,15 @@ local function ShouldShowSpecIcon(unitId) -- Return icon ID if should show, othe
             if ( not specID ) then return end
             local iconID, role = select(4, GetSpecializationInfoByID(specID));
 
-            if ( role == "HEALER" ) and SweepyBoop.db.profile.nameplatesEnemy.arenaSpecIconHealer then
-                return iconID;
-            elseif ( role ~= "HEALER" ) and SweepyBoop.db.profile.nameplatesEnemy.arenaSpecIconOthers then
+            if ( role == "HEALER" ) then
+                if config.arenaSpecIconHealer then
+                    if config.arenaSpecIconHealerIcon then
+                        return healerIconID;
+                    else
+                        return iconID;
+                    end
+                end
+            elseif config.arenaSpecIconOthers then
                 return iconID;
             end
         end
@@ -23,12 +36,26 @@ local function EnsureSpecIcon(frame)
     if ( not nameplate ) then return end
 
     if ( not nameplate.SpecIcon ) then
-        nameplate.SpecIcon = nameplate:CreateTexture(nil, 'overlay', nil, 6);
+        nameplate.SpecIcon = CreateFrame("Frame", nil, nameplate);
+        nameplate.SpecIcon:SetMouseClickEnabled(false);
         nameplate.SpecIcon:SetSize(iconSize, iconSize);
         nameplate.SpecIcon:SetPoint("BOTTOM", frame.name, "TOP");
-        nameplate.SpecIcon:SetAlpha(1);
-        nameplate.SpecIcon:SetIgnoreParentAlpha(true);
-        nameplate.SpecIcon:SetMask("interface/characterframe/tempportraitalphamask");
+        nameplate.SpecIcon:SetFrameStrata("HIGH");
+
+        nameplate.SpecIcon.icon = nameplate.SpecIcon:CreateTexture(nil, "BORDER");
+        nameplate.SpecIcon.icon:SetSize(iconSize, iconSize);
+        nameplate.SpecIcon.icon:SetPoint("CENTER", nameplate.SpecIcon);
+        
+        nameplate.SpecIcon.mask = nameplate.SpecIcon:CreateMaskTexture();
+        nameplate.SpecIcon.mask:SetAtlas("charactercreate-ring-metallight");
+        nameplate.SpecIcon.mask:SetSize(iconSize, iconSize);
+        nameplate.SpecIcon.mask:SetPoint("CENTER", nameplate.SpecIcon.icon);
+        nameplate.SpecIcon.icon:AddMaskTexture(nameplate.SpecIcon.mask);
+
+        -- nameplate.SpecIcon.border = nameplate.SpecIcon:CreateTexture(nil, "OVERLAY");
+        -- nameplate.SpecIcon.border:SetAtlas("charactercreate-ring-metallight");
+        -- nameplate.SpecIcon.border:SetSize(iconSize * 1.25, iconSize * 1.25);
+        -- nameplate.SpecIcon.border:SetPoint("CENTER", nameplate.SpecIcon);
 
         nameplate.SpecIcon:SetScale(SweepyBoop.db.profile.nameplatesEnemy.arenaSpecIconScale / 100);
         nameplate.SpecIcon.lastModified = SweepyBoop.db.profile.nameplatesEnemy.lastModified;
@@ -43,15 +70,20 @@ local function EnsureSpecIcon(frame)
 end
 
 local function ShowSpecIcon(frame, iconID)
-    local icon = EnsureSpecIcon(frame);
-    if ( not icon ) then return end;
+    local specIcon = EnsureSpecIcon(frame);
+    if ( not specIcon ) then return end;
 
-    if ( icon.iconID ~= iconID) then
-        icon:SetTexture(iconID);
-        icon.iconID = iconID;
+    if ( specIcon.iconID ~= iconID) then
+        specIcon.icon:SetTexture(iconID);
+        if ( iconID == healerIconID ) then
+            specIcon.icon:SetTexCoord(0.005, 0.116, 0.76, 0.87);
+        else
+            specIcon.icon:SetTexCoord(0, 1, 0, 1);
+        end
+        specIcon.iconID = iconID;
     end
 
-    icon:Show();
+    specIcon:Show();
 end
 
 addon.HideSpecIcon = function (frame)
