e42ef1ccb29d2434489b28ced2772b26c3f83236
Kun Liu <kunl@microsoft.com>
2024-12-22 00:11:36 -0800

    update default options panel size

--- Core.lua
+++ Core.lua
@@ -531,7 +531,7 @@ function SweepyBoop:OnInitialize()
     self.db = LibStub("AceDB-3.0"):New("SweepyBoopDB", defaults, true);
     options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db);
     local appName = LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options);
-    LibStub("AceConfigDialog-3.0"):SetDefaultSize(addonName, 800, 700);
+    LibStub("AceConfigDialog-3.0"):SetDefaultSize(addonName, 800, 600);
     self.optionsFrame, self.categoryID = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addonName, addon.addonTitle); -- Can we open to the friendly class icons page instead of the first empty page?
     
 
