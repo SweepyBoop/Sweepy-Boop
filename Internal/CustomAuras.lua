f3c519b978e1ab30139c72abafbb9fbb7465e5d8
Kun Liu <kunl@microsoft.com>
2024-12-23 01:24:36 -0800

    update

--- Internal/CustomAuras.lua
+++ Internal/CustomAuras.lua
@@ -343,7 +343,7 @@ local testGlowingBuffIcon = false;
 -- The first ActionBarButtonSpellActivationAlert created seems to be corrupted by other icons, so we create a dummy here that does nothing
 CreateFrame("Frame", nil, UIParent, "ActionBarButtonSpellActivationAlert");
 
-local precongnition = CreateGlowingBuffIcon(377362, 35, "CENTER", UIParent, "CENTER", 0, 60);
+CreateGlowingBuffIcon(377362, 35, "CENTER", UIParent, "CENTER", 0, 60); -- precongnition
 
 if ( class == addon.DRUID ) then
     CreateGlowingBuffIcon(5215, 64, "TOP", PlayerFrame.portrait, "BOTTOM", 0, -32); -- Prowl
