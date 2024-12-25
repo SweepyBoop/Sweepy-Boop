ac9d51fca7371400f667030c854fffcbe69c4042
Kun Liu <kunl@microsoft.com>
2024-12-25 12:39:50 -0800

    update

--- Nameplates/SpecIcons.lua
+++ Nameplates/SpecIcons.lua
@@ -44,6 +44,7 @@ local function EnsureSpecIcon(frame)
         nameplate.SpecIconFrame.frames = {};
         -- Subsequent calls to SetPoint with different anchor family will not work, we have to create one frame for each alignment option
         for alignment, options in pairs(setPointOptions) do
+            -- Parent to nameplate.frame, not nameplate, since when the nameplate health bar is hidden we don't want to show the spec icon
             nameplate.SpecIconFrame.frames[alignment] = addon.CreateClassOrSpecIcon(frame, options.point, options.relativePoint);
         end
     end
