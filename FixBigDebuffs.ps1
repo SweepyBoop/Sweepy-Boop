589bc9d5af71a14b7e8b5b061674a5c1efef9016
Kun Liu <kunl@microsoft.com>
2024-12-25 19:00:14 -0800

    update fix BigDebuffs script

--- FixBigDebuffs.ps1
+++ FixBigDebuffs.ps1
@@ -1,4 +1,3 @@
-$workDir = $PSScriptRoot
 $overrideFile = Join-Path -Path $PSScriptRoot -ChildPath "Internal\BigDebuffsOverride.lua"
 $addonDir = "D:\World of Warcraft\_retail_\Interface\Addons\BigDebuffs"
 $spellFile = Join-Path -Path $addonDir -ChildPath "BigDebuffs_Mainline.lua"
@@ -7,19 +6,25 @@ $spellFile = Join-Path -Path $addonDir -ChildPath "BigDebuffs_Mainline.lua"
 if (Test-Path $spellFile) {
     # Read the content of both files
     $spellFileContent = Get-Content $spellFile
-    $overrideFileFirstLine = (Get-Content $overrideFile -First 1).Trim()
+    $overrideFileFirstLine = Get-Content $overrideFile -First 1
     
-    # Check if the first line of $overrideFile is in $spellFile
-    if (-not ($spellFileContent -contains $overrideFileFirstLine)) {
-        Write-Host "Spell Overrides are missing in $spellFile. Appending content..."
+    # Check if the first line of $overrideFile exists in $spellFile
+    $lineIndex = $spellFileContent.IndexOf($overrideFileFirstLine)
+    if ($lineIndex -ge 0) {
+        Write-Host "The first line of $overrideFile exists in $spellFile. Removing it and all subsequent lines..."
         
-        # Append the content of $overrideFile to $spellFile
-        Get-Content $overrideFile | Add-Content $spellFile
+        # Remove all lines from the first occurrence of the line onwards
+        $updatedContent = $spellFileContent[0..($lineIndex - 1)]
         
-        Write-Host "Content from $overrideFile has been appended successfully."
-    } else {
-        Write-Host "Spell Overrides already exists in $spellFile. No changes made."
+        # Overwrite the $spellFile with the updated content
+        $updatedContent | Set-Content $spellFile
+        Write-Host "Lines removed successfully."
     }
+    
+    # Append the content of $overrideFile to $spellFile
+    Write-Host "Appending content from $overrideFile to $spellFile..."
+    Get-Content $overrideFile | Add-Content $spellFile
+    Write-Host "Content from $overrideFile has been appended successfully."
 } else {
     Write-Host "Destination file does not exist."
 }
