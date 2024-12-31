585f30a15c026578ab1a8a21f610452cbf92ce6a
Kun Liu <kunl@microsoft.com>
2024-12-31 14:32:25 -0800

    update

--- Misc/QueueReminder.lua
+++ Misc/QueueReminder.lua
@@ -44,7 +44,12 @@ local function SetExpiresText()
     local seconds = GetBattlefieldPortExpiration(battlefieldId);
     if ( seconds <= 0 ) then seconds = 1 end
     local color = ( seconds > 20 and "20ff20" ) or ( seconds > 10 and "ffff00" ) or "ff0000"; -- green -> yellow -> red
-    local text = format("Expires in |cff%s%s|r", color, SecondsToTime(seconds)); -- Only the time portion will change color
+    local queueType = "Expires in";
+    local battlefieldStatus = GetBattleFieldStatus(battlefieldId);
+    if battlefieldStatus and battlefieldStatus.queueType then
+        queueType = battlefieldStatus.queueType + "expires in";
+    end
+    local text = format("%s in |cff%s%s|r", queueType, color, SecondsToTime(seconds)); -- Only the time portion will change color
 
     EnsureTimerText(PVPReadyDialog);
     PVPReadyDialog.label:SetText(""); -- Hide the original label
