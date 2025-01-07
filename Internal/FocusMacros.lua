5deb973a9bd08d0369d00bc6547fa58345933c46
Kun Liu <kunl@microsoft.com>
2025-01-06 21:48:05 -0800

    update

--- Internal/FocusMacros.lua
+++ Internal/FocusMacros.lua
@@ -34,26 +34,14 @@ classAbilities[addon.PALADIN] = {
 };
 
 local function GetFocusName()
-    local isArena = IsActiveBattlefieldArena();
-
-    if isArena then
-        local roles = {};
-
+    if IsActiveBattlefieldArena() then
         for i = 1, addon.MAX_ARENA_SIZE do
             local spec = GetArenaOpponentSpec(i);
             if spec then
-                roles[i] = select(5, GetSpecializationInfoByID(spec));
-            end
-            if ( roles[i] == "HEALER" ) then
-                -- Early return if healer is found
-                return "arena" .. i;
-            end
-        end
-
-        -- Healer is not found, find a tank
-        for i = 1, addon.MAX_ARENA_SIZE do
-            if roles[i] and ( roles[i] ~= "DAMAGER" ) then
-                return "arena" .. i;
+                local role = select(5, GetSpecializationInfoByID(spec));
+                if ( role == "HEALER" ) then
+                    return "arena" .. i;
+                end
             end
         end
     end
@@ -67,6 +55,8 @@ local commonPrefix = "#showtooltip\n/cast [@";
 local commonSuffix = "] ";
 
 local function updateMacros(focusName)
+    print("Updated focus macros to index " .. focusName);
+
     local class = select(2, UnitClass("player"));
     local abilities = classAbilities[class];
     if ( not abilities ) then return end
@@ -85,52 +75,6 @@ local function updateMacros(focusName)
     end
 end
 
-local function UpdateHighlightBorder(frame, show)
-    if ( not frame.HighlightBorder ) then
-        local highlightBorder = CreateFrame("Frame", nil, frame, "BackdropTemplate");
-        --highlightBorder:SetFrameStrata("HIGH");
-        local width, healthBarHeight = frame.HealthBar:GetSize();
-        local _, powerBarHeight = frame.PowerBar:GetSize();
-        highlightBorder:SetSize(width + 5, (healthBarHeight + powerBarHeight) * 2.25); -- For Blizz Target 100% scale with system default scale
-        highlightBorder:SetPoint("BOTTOM", frame.PowerBar, "BOTTOM");
-        highlightBorder:SetBackdrop( { bgFile="Interface\\ChatFrame\\ChatFrameBackground", tileSize=16, tile=true, edgeFile="Interface\\ChatFrame\\ChatFrameBackground", edgeSize=1 } );
-        highlightBorder:SetBackdropColor(0, 0, 0, 0); -- transparent main area
-        highlightBorder:SetBackdropBorderColor(255, 0, 0); -- red border
-
-        highlightBorder:Hide();
-        frame.HighlightBorder = highlightBorder;
-    end
-
-    if show then
-        frame.HighlightBorder:Show();
-    else
-        frame.HighlightBorder:Hide();
-    end
-end
-
-local function UpdateArenaFrame(frame, show)
-    UpdateHighlightBorder(frame, show);
-end
-
-local function updateArenaHighlight(focusName)
-    local frameIndex = nil;
-
-    if ( focusName == "focus" ) then
-        if addon.TEST_MODE then
-            frameIndex = 1;
-        end
-    else
-        frameIndex = tonumber(string.sub(focusName, -1, -1));
-    end
-
-    for i = 1, 3 do
-        local sArenaFrame = _G["sArenaEnemyFrame" .. i];
-        if sArenaFrame then
-            UpdateArenaFrame( sArenaFrame, (i == frameIndex) );
-        end
-    end
-end
-
 local function TryUpdateMacros()
     if (InCombatLockdown()) then
         -- Combat locked, wait for 6s to drop combat
@@ -138,7 +82,6 @@ local function TryUpdateMacros()
     else
         local focusName = GetFocusName();
         updateMacros(focusName);
-        updateArenaHighlight(focusName);
     end
 end
 
