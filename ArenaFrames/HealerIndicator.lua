41b99175a6b62ac4e6438bf685e2f82d7a7da95a
Kun Liu <kunl@microsoft.com>
2025-01-07 18:01:07 -0800

    update

--- ArenaFrames/HealerIndicator.lua
+++ ArenaFrames/HealerIndicator.lua
@@ -80,7 +80,7 @@ local function UpdateHealerIndicator()
         return;
     end
 
-    size = size / 3;
+    size = size / 2.5;
     healerIndicator:SetSize(size, size);
     healerIndicator.icon:SetSize(size, size);
     healerIndicator:SetScale(scale);
