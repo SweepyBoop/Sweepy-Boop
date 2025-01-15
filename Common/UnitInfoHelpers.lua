4ed9fab5ea0568fe23b5fa3605a159998820d249
Kun Liu <kunl@microsoft.com>
2025-01-15 00:41:33 -0800

    update

--- Common/UnitInfoHelpers.lua
+++ Common/UnitInfoHelpers.lua
@@ -159,6 +159,7 @@ refreshFrame:SetScript("OnEvent", function (self, event)
 
     -- Right after a loading screen, both UnitInBattleground and C_PvP.IsBattleground are false
     -- we might as well have request frame always showing and scanning every 2.5s, request if in BG
+    -- instead of show/hide request frame based on UnitInBattleground
 end)
 
 addon.GetBattlefieldSpecByPlayerGuid = function (guid)
