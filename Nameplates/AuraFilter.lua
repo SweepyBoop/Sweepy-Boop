fb2688046ad72002ba9cd67d5d54440b861134cc
Kun Liu <kunl@microsoft.com>
2025-01-26 23:04:47 -0800

    update

--- Nameplates/AuraFilter.lua
+++ Nameplates/AuraFilter.lua
@@ -217,6 +217,7 @@ addon.UpdateBuffsOverride = function(self, unit, unitAuraUpdateInfo, auraSetting
     self:Layout();
 end
 
+-- Issue: auras are filtered properly initially but as a fight goes on, auras that are supposed to be hidden show up again
 addon.OnNamePlateAuraUpdate = function (self, unit, unitAuraUpdateInfo)
     -- Copied from BlizzardInterfaceCode but checking ( not addon.UnitIsHostile ) instead of PlayerUtil.HasFriendlyReaction
     -- This function is only called on hostile units (factoring in Mind Control)
