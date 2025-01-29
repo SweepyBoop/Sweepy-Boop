68d172b1e8423a3fa87664bb164c8b13a7dd3230
Kun Liu <kunl@microsoft.com>
2025-01-28 16:26:53 -0800

    fix

--- Nameplates/SpecIcons.lua
+++ Nameplates/SpecIcons.lua
@@ -13,7 +13,7 @@ local function GetSpecIconInfo(unitId) -- Return icon ID if should show, otherwi
     if ( not UnitIsPlayer(unitId) ) then return end -- No spec icon on non-player units
 
     local config = SweepyBoop.db.profile.nameplatesEnemy;
-    if IsActiveBattlefieldArena() or ( UnitInBattleground(unitId) ~= nil ) then
+    if IsActiveBattlefieldArena() or ( UnitInBattleground("player") ~= nil ) then
         local specInfo = addon.GetBattlefieldSpecByPlayerGuid(UnitGUID(unitId));
         if ( not specInfo ) then return end
         if ( specInfo.role == "HEALER" ) then
