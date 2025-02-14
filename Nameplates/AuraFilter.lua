e6bf0b38acec1646275fd4e4ccd287e894e3a0b9
Kun Liu <kunl@microsoft.com>
2025-02-13 17:44:47 -0800

    update

--- Nameplates/AuraFilter.lua
+++ Nameplates/AuraFilter.lua
@@ -323,7 +323,7 @@ addon.UpdateBuffsOverride = function(self, unit, unitAuraUpdateInfo, auraSetting
                 buff.Border:SetColorTexture(0.0, 0.5, 0.0); -- Green border for other buffs
                 buff.Border:Show();
             elseif aura.customCategory == AURA_CATEGORY.CROWD_CONTROL then
-                buff.Border:SetColorTexture(0.5, 0, 0.5); -- Purple border for crowd control
+                buff.Border:SetColorTexture(1.0000, 0.6471, 0.0000); -- Orange border for crowd control
                 buff.Border:Show();
             else
                 buff.Border:Hide();
