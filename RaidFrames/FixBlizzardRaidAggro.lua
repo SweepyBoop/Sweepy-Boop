a3e7601c642a0bc0afaecf986552c2e4d57e9287
Kun Liu <kunl@microsoft.com>
2025-02-22 11:16:22 -0800

    update

--- RaidFrames/FixBlizzardRaidAggro.lua
+++ RaidFrames/FixBlizzardRaidAggro.lua
@@ -4,25 +4,83 @@ local _, addon = ...;
 -- Make sure you disable Interface -> Raid Profiles -> "Display Aggro Highlight", and do a /reload
 -- If that option is enabled, the following code will not run so we don't mess with the Blizzard PVE aggro
 
-local R, G, B = GetThreatStatusColor(3); -- Red
+local throttle = 0.01;
+local scale = 1.4 * 0.85;
 
 local function GetThreatCount(unit)
-    if ( not unit ) then return end
-    if ( not IsActiveBattlefieldArena() ) and ( not addon.TEST_MODE ) then return end
+    local count = 0;
 
-    if addon.TEST_MODE then
-        return UnitIsUnit(unit, "target");
-    else
-        local count = 0;
+    if ( not unit ) then return count end
 
-        for i = 1, addon.MAX_ARENA_SIZE do
-            if UnitIsUnit(unit, "arena" .. i .. "target") then
-                count = count + 1;
-                if ( count > 1 ) then
-                    return true; -- red when unit is being targeted by more than one enemy players
-                end
+    -- Comment out for retail release
+    -- if addon.TEST_MODE then
+    --     count = UnitIsUnit(unit, "target") and 2 or 0;
+    --     return count;
+    -- end
+
+    for i = 1, addon.MAX_ARENA_SIZE do
+        if UnitIsUnit(unit, "arena" .. i .. "target") then
+            count = count + 1;
+            if ( count > 1 ) then
+                return count;
             end
         end
+    end
+
+    return count;
+end
+
+local function ShowCustomAggroHighlight(frame)
+    if not frame.customAggroHighlight then
+        local customAggroHighlight = CreateFrame("Frame", nil, frame);
+        customAggroHighlight:SetAllPoints();
+
+        customAggroHighlight.ants = customAggroHighlight:CreateTexture(nil, "OVERLAY");
+        customAggroHighlight.ants:SetPoint("CENTER");
+        customAggroHighlight.ants:SetTexture([[Interface\SpellActivationOverlay\IconAlertAnts]]);
+
+        customAggroHighlight:SetScript("OnUpdate", function (self, elapsed)
+            AnimateTexCoords(self.ants, 256, 256, 48, 48, 22, elapsed, throttle);
+        end)
+        customAggroHighlight:Hide();
+
+        frame.customAggroHighlight = customAggroHighlight;
+    end
+
+    frame.customAggroHighlight.ants:SetSize(frame:GetWidth() * scale, frame:GetHeight() * scale);
+    frame.customAggroHighlight:Show();
+end
+
+local function ShowCustomAggroHighlightPurple(frame)
+    if not frame.customAggroHighlightPurple then
+        local customAggroHighlight = CreateFrame("Frame", nil, frame);
+        customAggroHighlight:SetAllPoints();
+
+        customAggroHighlight.ants = customAggroHighlight:CreateTexture(nil, "OVERLAY");
+        customAggroHighlight.ants:SetPoint("CENTER");
+        customAggroHighlight.ants:SetTexture([[Interface\Transmogrify\PurpleIconAlertAnts]]);
+
+        customAggroHighlight:SetScript("OnUpdate", function (self, elapsed)
+            AnimateTexCoords(self.ants, 256, 256, 48, 48, 22, elapsed, throttle);
+        end)
+        customAggroHighlight:Hide();
+
+        frame.customAggroHighlightPurple = customAggroHighlight;
+    end
+
+    frame.customAggroHighlightPurple.ants:SetSize(frame:GetWidth() * scale, frame:GetHeight() * scale);
+    frame.customAggroHighlightPurple:Show();
+end
+
+local function HideCustomAggroHighlight(frame)
+    if frame.customAggroHighlight then
+        frame.customAggroHighlight:Hide();
+    end
+end
+
+local function HideCustomAggroHighlightPurple(frame)
+    if frame.customAggroHighlightPurple then
+        frame.customAggroHighlightPurple:Hide();
     end
 end
 
@@ -33,14 +91,36 @@ function SweepyBoop:SetupRaidFrameAggroHighlight()
             frame.isParentCompactPartyFrame = ( frame:GetParent() == CompactPartyFrame );
         end
         if ( not frame.isParentCompactPartyFrame ) then return end
-        if ( not self.db.profile.raidFrames.raidFrameAggroHighlightEnabled ) then return end -- If feature disabled
-        if frame.optionTable.displayAggroHighlight then return end -- Don't overwrite PvE threats
+        if ( not self.db.profile.raidFrames.raidFrameAggroHighlightEnabled ) then -- If feature disabled
+            if frame.aggroHighlight then
+                frame.aggroHighlight:SetAlpha(1);
+            end
 
-        if GetThreatCount(frame.unit) then
-            frame.aggroHighlight:SetVertexColor(R, G, B);
-            frame.aggroHighlight:Show();
+            return;
+        end
+
+        -- Comment out when testing
+        if ( not IsActiveBattlefieldArena() ) then
+            if frame.aggroHighlight then
+                frame.aggroHighlight:SetAlpha(1);
+            end
+
+            HideCustomAggroHighlight(frame);
+            HideCustomAggroHighlightPurple(frame);
+            return;
+        end
+
+        local threatCount = GetThreatCount(frame.unit);
+
+        if threatCount > 1 then
+            HideCustomAggroHighlight(frame);
+            ShowCustomAggroHighlightPurple(frame);
+        elseif threatCount > 0 then
+            HideCustomAggroHighlightPurple(frame);
+            ShowCustomAggroHighlight(frame);
         else
-            frame.aggroHighlight:Hide();
+            HideCustomAggroHighlight(frame);
+            HideCustomAggroHighlightPurple(frame);
         end
     end)
 end
