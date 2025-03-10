da74f235b42a0f14ea69d3c12ed71357af746348
Kun Liu <kunl@microsoft.com>
2025-03-09 21:51:51 -0700

    icon is not showing yet

--- Misc/CombatIndicator.lua
+++ Misc/CombatIndicator.lua
@@ -6,14 +6,45 @@ local unitToFrame = {
     focus = FocusFrame
 };
 
-local function UpdateCombatIndicator(frame, unit)
+local unitToPortrait;
+if addon.PROJECT_MAINLINE then
+    unitToPortrait = {
+        player = PlayerFrame.PlayerFrameContainer.PlayerPortrait,
+        target = TargetFrame.TargetFrameContainer.Portrait,
+        focus = FocusFrame.TargetFrameContainer.Portrait,
+    };
+else
+    unitToPortrait = {
+        player = PlayerFrame.portraitOverlay,
+        target = TargetFrame.portraitOverlay,
+        focus = FocusFrame.portraitOverlay
+    };
 end
 
-local function UpdateAllIndicators(frame, unit)
+local anchorPoint = {
+    player = "BOTTOMLEFT",
+    target = "BOTTOMRIGHT",
+    focus = "BOTTOMRIGHT",
+};
+
+local function UpdateCombatIndicator(frame, unit)
+    if ( not frame.combatIndicator ) then
+        frame.combatIndicator = CreateFrame("Frame", nil, frame);
+        frame.combatIndicator:SetMouseClickEnabled(false);
+        frame.combatIndicator:SetFrameStrata("HIGH");
+        frame.combatIndicator:SetPoint(anchorPoint[unit], unitToPortrait[unit]);
+
+        frame.combatIndicator.icon = frame.combatIndicator:CreateTexture(nil, "OVERLAY");
+        frame.combatIndicator.icon:SetSize(32, 32);
+        frame.combatIndicator.icon:SetAtlas("countdown-swords");
+        frame.combatIndicator.icon:SetAllPoints();
+    end
+
+    frame.combatIndicator:Show();
 end
 
 local frame = CreateFrame("Frame");
-frame:Hide(); -- When hidden it won't process events
+--frame:Hide(); -- When hidden it won't process events
 frame:RegisterUnitEvent(addon.UNIT_FLAGS, "player", "target", "focus");
 frame:RegisterEvent(addon.PLAYER_ENTERING_WORLD);
 frame:RegisterEvent(addon.PLAYER_TARGET_CHANGED);
@@ -26,7 +57,7 @@ frame:SetScript("OnEvent", function(self, event, unit)
         end
     else
         for unit, frame in pairs(unitToFrame) do
-            UpdateAllIndicators(frame, unit);
+            UpdateCombatIndicator(frame, unit);
         end
     end
 end);
