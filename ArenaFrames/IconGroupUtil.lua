856e66a572d6cfe6b2fe4ef61bea32d7475b947e
Kun Liu <kunl@microsoft.com>
2025-03-18 12:20:47 -0700

    update

--- ArenaFrames/IconGroupUtil.lua
+++ ArenaFrames/IconGroupUtil.lua
@@ -8,7 +8,14 @@ addon.CreateIconGroup = function (setPointOptions, growOptions, unit)
 
     local f = CreateFrame("Frame", nil, UIParent);
     f:SetSize(1, 1);
-    f.setPointOptions = setPointOptions;
+
+    local relativeToFrame = _G[relativeTo];
+    if relativeToFrame and relativeToFrame:IsShown() then
+        f:ClearAllPoints();
+        f:SetPoint(point, relativeTo, relativePoint, offsetX, offsetY);
+    else
+        f.setPointOptions = setPointOptions;
+    end
 
     -- e.g., grow = "LEFT", growAnchor = "BOTTOMRIGHT": set icon's bottomright to group's bottom right
     f.growDirection = growOptions.direction;
@@ -31,12 +38,12 @@ addon.UpdateIconGroupSetPointOptions = function (iconGroup, setPointOptions, gro
     local point, relativeTo, relativePoint, offsetX, offsetY =
         setPointOptions.point, setPointOptions.relativeTo, setPointOptions.relativePoint, setPointOptions.offsetX, setPointOptions.offsetY;
 
-    iconGroup.setPointOptions = setPointOptions;
-    -- If relativeTo is currently shown, update the position now
     local relativeToFrame = _G[relativeTo];
     if relativeToFrame and relativeToFrame:IsShown() then
-        --iconGroup:ClearAllPoints();
+        iconGroup:ClearAllPoints();
         iconGroup:SetPoint(point, relativeTo, relativePoint, offsetX, offsetY);
+    else
+        iconGroup.setPointOptions = setPointOptions;
     end
 
     iconGroup.growDirection = growOptions.direction;
@@ -119,7 +126,7 @@ addon.IconGroup_Insert = function (group, icon, index)
     -- Re-adjust positioning if this group attaches to an arena frame, since arena frames can change position
     if group.setPointOptions then
         local options = group.setPointOptions;
-        --group:ClearAllPoints();
+        group:ClearAllPoints();
         group:SetPoint(options.point, options.relativeTo, options.relativePoint, options.offsetX, options.offsetY);
         group.setPointOptions = nil; -- Don't need to do this again until updated by UpdateIconGroupSetPointOptions
     end
