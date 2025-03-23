83e42b42f79f2c3e33c12f74902b470444e34d27
Kun Liu <kunl@microsoft.com>
2025-03-23 01:48:48 -0700

    update

--- ArenaFrames/IconGroupUtil.lua
+++ ArenaFrames/IconGroupUtil.lua
@@ -153,7 +153,12 @@ addon.IconGroup_Insert = function (group, icon, index)
     --print(group:GetPoint());
 end
 
-addon.IconGroup_Remove = function (group, icon)
+addon.IconGroup_Remove = function (group, icon, fade)
+    if fade then
+        icon:SetAlpha(SweepyBoop.db.profile.arenaFrames.unusedIconAlpha);
+        return;
+    end
+
     -- Hide icon first, then reposition, to avoid occlusion.
     icon:Hide();
 
