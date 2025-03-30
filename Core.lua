d0c005df9e0433579f5e076a7bc1f105f69bfe7b
Kun Liu <kunl@microsoft.com>
2025-03-30 14:56:52 -0700

    update

--- Core.lua
+++ Core.lua
@@ -1165,7 +1165,7 @@ function SweepyBoop:OnInitialize()
 
     self.db = LibStub("AceDB-3.0"):New("SweepyBoopDB", defaults, true);
     options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db);
-    local appName = LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options);
+    LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options);
     LibStub("AceConfigDialog-3.0"):SetDefaultSize(addonName, 750, 640);
     self.optionsFrame, self.categoryID = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addonName, addon.addonTitle); -- Can we open to the friendly class icons page instead of the first empty page?
 
