b1bb32589275721449e83276df30e87957afd629
Kun Liu <kunl@microsoft.com>
2025-03-31 13:24:10 -0700

    update logic for comp

--- Nameplates/ClassIcons.lua
+++ Nameplates/ClassIcons.lua
@@ -155,7 +155,8 @@ addon.UpdateClassIconCrowdControl = function(nameplate, frame)
                 local category = addon.DRList[auraData.spellId];
                 local update = false;
                 if crowdControlPriority[category] then -- Found a CC that should be shown
-                    if ( not expirationTime ) or ( not auraData.expirationTime ) or ( auraData.expirationTime < expirationTime) then -- first compare by expirationTime
+                    -- No expirationTime means this aura never expires, so it should be prioritized
+                    if ( not auraData.expirationTime ) or ( expirationTime and auraData.expirationTime and auraData.expirationTime < expirationTime ) then
                         update = true;
                     elseif crowdControlPriority[category] > priority then -- same expirationTime, use priority as tie breaker
                         update = true;
