75f98353f19b6fa1506c3f9fe68ee8a5e9c55251
Kun Liu <kunl@microsoft.com>
2025-03-31 19:52:08 -0700

    update

--- Internal/CpuUsage.lua
+++ Internal/CpuUsage.lua
@@ -4,17 +4,35 @@ local lastUpdated;
 
 local eventFrame = CreateFrame("Frame");
 eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
-eventFrame:SetScript("OnEvent", function ()
-    local now = GetTime();
-    if lastUpdated then
-        local elapsed = now - lastUpdated;
-        UpdateAddOnCPUUsage();
+eventFrame:RegisterEvent("PVP_MATCH_COMPLETE");
+eventFrame:SetScript("OnEvent", function(_, event)
+    if ( event == "PLAYER_ENTERING_WORLD" ) then
+        ResetCPUUsage();
+        lastUpdated = GetTime();
+    elseif ( event == "PVP_MATCH_COMPLETE" ) then
+        local now = GetTime();
+        if lastUpdated then
+            local elapsed = now - lastUpdated;
+            UpdateAddOnCPUUsage();
 
-        -- ms spent per second
-        local cpuUsage = GetAddOnCPUUsage("SweepyBoop") / elapsed;
-        local cpuBigDebuffs = GetAddOnCPUUsage("BigDebuffs") / elapsed;
-        print("Addon usage:", cpuUsage, "BigDebuffs usage:", cpuBigDebuffs);
+            local addonInfo = {}; -- each entry contains name and ms spent per Sec
+            for i = 1, GetNumAddOns() do
+                local name = C_AddOns.GetAddOnInfo(i);
+                local cpuUsage = string.format("%.2f", GetAddOnCPUUsage(i) / elapsed);
+                table.insert(addonInfo, { name = name, cpuUsage = cpuUsage });
+            end
+
+            -- sort by CPU usage
+            table.sort(addonInfo, function(a, b) return a.cpuUsage > b.cpuUsage end);
+
+            -- print top 3 add-ons
+            print("Top 3 Addons by CPU Usage in last PvP match:");
+            for i = 1, math.min(3, #addonInfo) do
+                local info = addonInfo[i];
+                print(info.name .. ": " .. info.cpuUsage .. " ms/sec");
+            end
+        end
+        ResetCPUUsage();
+        lastUpdated = now;
     end
-    ResetCPUUsage();
-    lastUpdated = now;
 end)
