2181c9093b8eabfafe1c7194b6e711449d9f94ca
Kun Liu <kunl@microsoft.com>
2025-04-01 03:01:04 -0700

    update

---
+++ ArenaFrames/InterruptBar.lua
@@ -0,0 +1,22 @@
+local _, addon = ...;
+
+local spellData = addon.spellData;
+local spellResets = addon.spellResets;
+
+local premadeIcons = {};
+local iconGroup = {};
+local eventFrame;
+
+for spellID, spell in pairs(spellData) do
+    -- Fill default priority
+    spell.priority = spell.index or addon.SPELLPRIORITY.DEFAULT;
+
+    if ( not spell.class ) or ( not C_Spell.GetSpellName(spellID) ) then
+        print("Invalid spellID:", spellID);
+    end
+
+    -- Class should be a string of capital letters
+    if spell.class and ( type(spell.class) ~= "string" ) then
+        print("Invalid class for spellID:", spellID);
+    end
+end
