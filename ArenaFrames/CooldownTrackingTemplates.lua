3e4969e1def4d575785d7b34b565fab2aca8834e
Kun Liu <kunl@microsoft.com>
2025-04-04 03:08:01 -0700

    remove debug str

--- ArenaFrames/CooldownTrackingTemplates.lua
+++ ArenaFrames/CooldownTrackingTemplates.lua
@@ -42,7 +42,6 @@ function CooldownTracking_UpdateAlpha(icon)
             usedIconAlpha = 1;
         end
     end
-    print("CooldownTracking_UpdateAlpha", config.interruptBarUsedIconAlpha, config.usedIconAlpha, "Set to", usedIconAlpha);
     icon:SetAlpha(usedIconAlpha);
 end
 
