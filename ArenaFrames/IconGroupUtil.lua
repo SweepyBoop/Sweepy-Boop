8715e8a0b7d2bb0c8b7a947c4a642b7a46ba27c7
Kun Liu <kunl@microsoft.com>
2025-04-07 00:34:03 -0700

    fix interrupt layout when solar beam is present

--- ArenaFrames/IconGroupUtil.lua
+++ ArenaFrames/IconGroupUtil.lua
@@ -80,8 +80,8 @@ addon.IconGroup_Position = function(group)
     local separateRowForInterrupts = group.isInterruptBar and SweepyBoop.db.profile.arenaFrames.separateRowForInterrupts;
     if separateRowForInterrupts then
         for i = 1, numActive do
-            interruptCount = interruptCount + ( group.active[i].category == INTERRUPT and 1 or 0 );
-            otherCount = otherCount + ( group.active[i].category ~= INTERRUPT and 1 or 0 );
+            interruptCount = interruptCount + ( group.active[i].isInterrupt and 1 or 0 );
+            otherCount = otherCount + ( ( not group.active[i].isInterrupt ) and 1 or 0 );
         end
     end
 
