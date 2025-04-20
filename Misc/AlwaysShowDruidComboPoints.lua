00abb7e14927a6eb4e094e0a58010648eb2e21ca
Kun Liu <kunl@microsoft.com>
2025-04-20 00:51:57 -0700

    update

--- Misc/AlwaysShowDruidComboPoints.lua
+++ Misc/AlwaysShowDruidComboPoints.lua
@@ -25,24 +25,32 @@ local function UpdateComboPoints(frame)
     end
 end
 
+local comboPointFrame;
+local eventFrame;
+
 function SweepyBoop:SetupAlwaysShowDruidComboPoints()
     if ( addon.GetUnitClass("player") ~= addon.DRUID ) then return end
 
-    local comboPointFrame = DruidComboPointBarFrame;
+    if ( not comboPointFrame ) then -- Make sure to only init once
+        comboPointFrame = DruidComboPointBarFrame;
 
-    comboPointFrame:HookScript("OnHide", function(frame)
-        if ( not SweepyBoop.db.profile.misc.alwaysShowDruidComboPoints ) then return end
-        if ( UnitPower("player", frame.powerType) > 0 ) then
-            frame:Show();
-        end
-    end);
+        comboPointFrame:HookScript("OnHide", function(frame)
+            if ( not SweepyBoop.db.profile.misc.alwaysShowDruidComboPoints ) then return end
+            if ( UnitPower("player", frame.powerType) > 0 ) then
+                frame:Show();
+            end
+        end);
+    end
 
-    local eventFrame = CreateFrame("Frame");
-    eventFrame:SetScript("OnEvent", function(_, _, _, powerType)
-        if ( powerType == "COMBO_POINTS" ) then
-            UpdateComboPoints(comboPointFrame);
-        end
-    end);
+    if ( not eventFrame ) then -- Make sure to only create eventFrame once
+        eventFrame = CreateFrame("Frame");
+        eventFrame:SetScript("OnEvent", function(_, _, _, powerType)
+            if ( powerType == "COMBO_POINTS" ) then
+                UpdateComboPoints(comboPointFrame);
+            end
+        end);
+    end
+
     if SweepyBoop.db.profile.misc.alwaysShowDruidComboPoints then
         eventFrame:RegisterEvent(addon.UNIT_POWER_UPDATE);
         UpdateComboPoints(comboPointFrame); -- Do one-off initial update
