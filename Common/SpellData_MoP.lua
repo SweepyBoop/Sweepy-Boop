fa03d8d87a00e5363d518c9c8b6c4a08fbf47cde
Kun Liu <kunl@microsoft.com>
2025-06-23 01:24:20 -0700

    update

--- Common/SpellData_MoP.lua
+++ Common/SpellData_MoP.lua
@@ -1652,103 +1652,15 @@ addon.SpellData = {
 };
 
 addon.SpellResets = {
-    -- Metamorphosis
-    [191427] = {
-        198013, -- Eye Beam
-    },
-    -- Blade Dance
-    [188499] = {
-        { spellID = 198013, amount = 1 }, -- Eye Beam
-    },
-    -- Chaos Strike
-    [162794] = {
-        { spellID = 198013, amount = 1 }, -- Eye Beam
-    },
-    -- Glaive Tempest
-    [342817] = {
-        { spellID = 198013, amount = 1 }, -- Eye Beam
-    },
-
-    -- Bestial Wrath
-    -- Barbed Shot
-    [217200] = {
-        { spellID = 19574, amount = 12 }, -- Bestial Wrath
-    },
-
     -- Cold Snap
-    [235219] = {
+    [11958] = {
         45438, -- Ice Block
     },
 
-    -- Apotheosis
-    [200183] = {
-        88625, -- Holy Word: Chastise
-        2050, -- Grants one charge of Holy Word: Serenity
-    },
+    -- Call of the Elements
+    [108285] = {
 
-    -- Holy Fire
-    [14914] = {
-        { spellID = 88625, amount = 4 }, -- Holy Word: Chastise
     },
-    -- Smite
-    [585] = {
-        { spellID = 88625, amount = 4 }, -- Holy Word: Chastise
-    },
-    -- Holy Nova
-    [132157] = {
-        { spellID = 88625, amount = 4 }, -- Holy Word: Chastise
-    },
-
-    -- Prayer of Mending
-    [33076] = {
-        { spellID = 2050, amount = 4 }, -- Holy Word: Serenity
-    },
-    -- Power Word: Life
-    [440678] = {
-        { spellID = 2050, amount = 4 }, -- Holy Word: Serenity
-    },
-    -- Heal
-    [2060] = {
-        { spellID = 2050, amount = 6 }, -- Holy Word: Serenity
-    },
-    -- Flash Heal
-    [2061] = {
-        { spellID = 2050, amount = 6 }, -- Holy Word: Serenity
-    },
-
-    -- Power Word: Shield
-    [17] = {
-        { spellID = 33206, amount = 3 }, -- Pain Suppression
-    },
-
-    -- Kill Command
-    [34026] = {
-        { spellID = 19577, amount = 0.5 }, -- Intimidation
-        { spellID = 109248, amount = 0.5 }, -- Binding Shot
-    }
-};
-
-addon.SpellResets[382445] = {}; -- Shifting Power each tick
-addon.SpellResets[157982] = {}; -- Tranquility each tick
--- Reduce each Mage spell by 3 Sec
--- Fill the entries programatically from addon.Spells
-for spellID, spell in pairs(addon.SpellData) do
-    if spell.class == addon.MAGE then
-        table.insert(addon.SpellResets[382445], { spellID = spellID, amount = 3 });
-    elseif ( spell.class == addon.DRUID and spellID ~= 740 ) then
-        table.insert(addon.SpellResets[157982], { spellID = spellID, amount = 4 });
-    end
-end
-
-addon.SpellResetsAffectedByApotheosis = {
-    [14914] = 2, -- Holy Fire (nerfed in PvP)
-    [585] = 3, -- Smite
-    [132157] = 3, -- Holy Nova
-
-    [33076] = 3, -- Prayer of Mending
-    [440678] = 3, -- Power Word: Life
-    [2060] = 3, -- Heal
-    [2061] = 3, -- Flash Heal
 };
 
 for _, spell in pairs(addon.SpellData) do
