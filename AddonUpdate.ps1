5b2e42a6a050597e2fa61d3b272abe5b569b8006
Kun Liu <kunl@microsoft.com>
2025-07-01 16:59:41 -0700

    update scripts

--- AddonUpdate.ps1
+++ AddonUpdate.ps1
@@ -1,7 +1,6 @@
 $workDir = $PSScriptRoot
 $addonDir = "D:\World of Warcraft\_retail_\Interface\Addons\SweepyBoop"
 $classicDir = "D:\World of Warcraft\_classic_\Interface\Addons\SweepyBoop"
-$mopDir = "D:\World of Warcraft\_classic_beta_\Interface\Addons\SweepyBoop"
 
 if (Test-Path -Path $addonDir) {
     Remove-Item -Path $addonDir -Recurse -Force
@@ -9,12 +8,8 @@ if (Test-Path -Path $addonDir) {
 if (Test-Path -Path $classicDir) {
     Remove-Item -Path $classicDir -Recurse -Force
 }
-if (Test-Path -Path $mopDir) {
-    Remove-Item -Path $mopDir -Recurse -Force
-}
 New-Item -ItemType Directory -Path $addonDir | Out-Null
 New-Item -ItemType Directory -Path $classicDir | Out-Null
-New-Item -ItemType Directory -Path $mopDir | Out-Null
 
 $extensions = @("*.lua", "*.toc", "*.xml")
 foreach ($ext in $extensions) {
@@ -22,9 +17,6 @@ foreach ($ext in $extensions) {
 }
 foreach ($ext in $extensions) {
     Copy-Item -Path "$PSScriptRoot\$ext" -Destination "$classicDir" -Force
-}
-foreach ($ext in $extensions) {
-    Copy-Item -Path "$PSScriptRoot\$ext" -Destination "$mopDir" -Force
 }
 
 $excludePatterns = @("*.git*", "*Docs*")
@@ -37,16 +29,9 @@ foreach ($dir in $dirsToCopy) {
     $destPath = Join-Path -Path $classicDir -ChildPath $dir.Name
     Copy-Item -Path $dir.FullName -Destination $destPath -Recurse -Force
 }
-foreach ($dir in $dirsToCopy) {
-    $destPath = Join-Path -Path $mopDir -ChildPath $dir.Name
-    Copy-Item -Path $dir.FullName -Destination $destPath -Recurse -Force
-}
 
 $constantsFileRetail = Join-Path -Path $addonDir -ChildPath "Common\Constants.lua"
 "addon.internal = true;" | Out-File -FilePath $constantsFileRetail -Append -Encoding UTF8
 
 $constantsFileClassic = Join-Path -Path $classicDir -ChildPath "Common\Constants.lua"
 "addon.internal = true;" | Out-File -FilePath $constantsFileClassic -Append -Encoding UTF8
-
-$constantsFileMop = Join-Path -Path $mopDir -ChildPath "Common\Constants.lua"
-"addon.internal = true;" | Out-File -FilePath $constantsFileMop -Append -Encoding UTF8
