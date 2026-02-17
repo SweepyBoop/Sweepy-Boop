2fcbd03fcc80e2f418c833d6783953b502d168b9
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2026-02-16 21:56:45 -0800

    Update Nameplates.lua

--- Nameplates/Nameplates.lua
+++ Nameplates/Nameplates.lua
@@ -28,7 +28,6 @@ end
 local function IsForbiddenSafe(frame)
     -- local success, result = pcall(function() return frame:IsForbidden() end);
     -- return ( not success ) or result;
-    if ( not frame ) then return true end
     if issecretvalue(frame) then return true end
     return frame:IsForbidden();
 end
